397be63433b967f3e7320a5ece28190bcc0d931e
BTroolin <61754208+BTroolin@users.noreply.github.com>
2021-03-07 12:12:47 -0600

    markers and axes

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,4 +53,58 @@ function render_text(circletext_grp, new_x_scale, new_y_scale, chosen_x, chosen_
 }
 
 // update circles with new tooltip
-function update_tooltip(chosen_x, chosen_y)
+function update_tooltip(chosen_x, chosen_y, circle_group, text_group) {
+    // if/else for chosen axes
+    if (chosen_x === "poverty") {
+        var X_label = "Poverty: ";
+    } else if (chosen_x === "income") {
+        var X_label = "Median Income: "
+    } else {
+        var X_label = "Age: "
+    }
+    //repeat for y axis
+    if (chosen_y === "healthcare") {
+        var Y_label = "No Healthcare: ";
+    } else if (chosen_y === "smokes") {
+        var Y_label = "Smokers: "
+    } else {var Y_label = "Obesity: "}
+    // set up tooltip
+    var tool_tip = d3.tip()
+        .offset([100, -50])
+        .attr("class", "d3-tip")
+        .html(function(d) {
+            if (chosen_x === "age") {
+                return (`${d.state}<hr>${X_label}${d[chosen_x]}<br>${Y_label}${d[chosen_y]}%`);
+                }
+                else if (chosen_x === "income") {
+                return (`${d.state}<hr>${X_label}$${d[chosen_x]}<br>${Y_label}${d[chosen_y]}%`)
+                }
+                else {
+                return (`${d.state}<hr>${X_label}${d[chosen_x]}%<br>${Y_label}${d[chosen_y]}%`)
+                }
+        });
+        circle_group.call(toolTip);
+
+        circle_group
+            .on("mouseover", function(data) {toolTip.show(data);})
+            .on("mouseout", function(data) {toolTip.hide(data);});
+        text_group
+            .on("mouseover", function(data) {toolTip.show(data);})
+            .on("mouseout", function(data) {tooltip.hide(data);});
+        return circle_group;
+}
+function make_responsive() {
+    var svg_area = d3.select("#scatter").select("svg");
+    // clear out the svg
+    if (!svg_area.empty()) {svg_area.remove();}
+    //set parameters
+    var svg_height = window.innerHeight/1.2;
+    var svg_width = window.innerWidth/1.7;
+    var margins = {
+        top:50,
+        right: 50,
+        bottom: 100,
+        left: 80
+    };
+
+}
